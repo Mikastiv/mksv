784329247e6c0812315b73919ac7889d2bd3dd3f
Mikastiv <mikastiv@outlook.com>
2024-04-27 15:54:20 -0400

    bit stream

--- src/main.zig
+++ src/main.zig
@@ -11,37 +11,72 @@ const FiberData = struct {
 };
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-    _ = allocator; // autofix
+    // const allocator = std.heap.page_allocator;
+    // _ = allocator; // autofix
 
-    const T = mksv.avl.Tree(u32, less);
-    var tree = T{};
+    // const T = mksv.avl.Tree(u32, less);
+    // var tree = T{};
 
     var rng = std.Random.DefaultPrng.init(0);
     const rand = rng.random();
 
-    var nodes = [10]T.Node{
-        .{ .value = rand.int(u32) % 64 },
-        .{ .value = rand.int(u32) % 64 },
-        .{ .value = rand.int(u32) % 64 },
-        .{ .value = rand.int(u32) % 64 },
-        .{ .value = rand.int(u32) % 64 },
-        .{ .value = rand.int(u32) % 64 },
-        .{ .value = rand.int(u32) % 64 },
-        .{ .value = rand.int(u32) % 64 },
-        .{ .value = rand.int(u32) % 64 },
-        .{ .value = rand.int(u32) % 64 },
-    };
+    // var nodes = [10]T.Node{
+    //     .{ .value = rand.int(u32) % 64 },
+    //     .{ .value = rand.int(u32) % 64 },
+    //     .{ .value = rand.int(u32) % 64 },
+    //     .{ .value = rand.int(u32) % 64 },
+    //     .{ .value = rand.int(u32) % 64 },
+    //     .{ .value = rand.int(u32) % 64 },
+    //     .{ .value = rand.int(u32) % 64 },
+    //     .{ .value = rand.int(u32) % 64 },
+    //     .{ .value = rand.int(u32) % 64 },
+    //     .{ .value = rand.int(u32) % 64 },
+    // };
 
-    _ = tree.insert(&nodes[0]);
-    _ = tree.insert(&nodes[1]);
-    _ = tree.insert(&nodes[2]);
-    _ = tree.insert(&nodes[3]);
-    _ = tree.insert(&nodes[4]);
-    _ = tree.insert(&nodes[5]);
-    _ = tree.insert(&nodes[6]);
-    _ = tree.insert(&nodes[7]);
-    _ = tree.insert(&nodes[8]);
+    // _ = tree.insert(&nodes[0]);
+    // _ = tree.insert(&nodes[1]);
+    // _ = tree.insert(&nodes[2]);
+    // _ = tree.insert(&nodes[3]);
+    // _ = tree.insert(&nodes[4]);
+    // _ = tree.insert(&nodes[5]);
+    // _ = tree.insert(&nodes[6]);
+    // _ = tree.insert(&nodes[7]);
+    // _ = tree.insert(&nodes[8]);
 
-    try tree.debugPrint();
+    // try tree.debugPrint();
+
+    var bytes: [3]u8 = undefined;
+    rand.bytes(&bytes);
+
+    for (bytes) |b| {
+        std.debug.print("{b:0>8} ", .{b});
+    }
+    std.debug.print("\n", .{});
+
+    var buf_stream = std.io.fixedBufferStream(&bytes);
+
+    var stream = mksv.bit_stream.BitStream(@TypeOf(buf_stream).Reader, .big).init(buf_stream.reader());
+
+    std.debug.print("{b}", .{try stream.readBits(1)});
+    std.debug.print("{b}", .{try stream.readBits(1)});
+    std.debug.print("{b:0>5}", .{try stream.readBits(5)});
+    std.debug.print("{b}\n", .{try stream.readBits(1)});
+    std.debug.print("{b}", .{try stream.readBits(1)});
+    std.debug.print("{b}", .{try stream.readBits(1)});
+    std.debug.print("{b}", .{try stream.readBits(1)});
+    std.debug.print("{b}", .{try stream.readBits(1)});
+    std.debug.print("{b}", .{try stream.readBits(1)});
+    std.debug.print("{b}", .{try stream.readBits(1)});
+    std.debug.print("{b}", .{try stream.readBits(1)});
+    std.debug.print("{b}\n", .{try stream.readBits(1)});
+    std.debug.print("{b}", .{try stream.readBits(1)});
+    std.debug.print("{b:0>7}\n", .{try stream.readBits(7)});
+
+    buf_stream.reset();
+    var stream_little = mksv.bit_stream.StaticStream(@TypeOf(buf_stream).Reader, .little).init(buf_stream.reader());
+
+    std.debug.print("{b}", .{try stream_little.readBits(1)});
+    std.debug.print("{b}", .{try stream_little.readBits(1)});
+    std.debug.print("{b:0>5}", .{try stream_little.readBits(5)});
+    std.debug.print("{b}\n", .{try stream_little.readBits(1)});
 }
---
+++ src/stream.zig
@@ -0,0 +1,102 @@
+const std = @import("std");
+
+const assert = std.debug.assert;
+
+pub fn BitStream(comptime Reader: type, comptime endian: std.builtin.Endian) type {
+    return struct {
+        const Self = @This();
+
+        reader: Reader,
+        byte: ?u8 = null,
+        bit_index: u8 = if (endian == .big) 7 else 0,
+
+        pub fn init(reader: Reader) Self {
+            return .{
+                .reader = reader,
+            };
+        }
+
+        pub fn readBits(self: *Self, count: u4) !u8 {
+            assert(count > 0 and count <= 8);
+
+            if (self.byte == null) {
+                self.byte = try self.reader.readByte();
+            }
+
+            const bits_remaining = self.bitsRemaining();
+            assert(count <= bits_remaining);
+
+            const byte = self.byte.?;
+
+            const shift: u3 = @intCast(if (endian == .big) self.bit_index - (count - 1) else self.bit_index);
+            const mask: u8 = @intCast(((@as(u16, 1) << count) - 1) << shift);
+            const value = (byte & mask) >> shift;
+
+            if (endian == .big) {
+                if (bits_remaining - count == 0) {
+                    self.byte = null;
+                    self.bit_index = 7;
+                } else {
+                    self.bit_index -= count;
+                }
+            } else {
+                if (bits_remaining - count == 0) {
+                    self.byte = null;
+                    self.bit_index = 0;
+                } else {
+                    self.bit_index += count;
+                }
+            }
+
+            return if (endian == .big)
+                value
+            else
+                @bitReverse(value) >> @intCast(8 - count);
+        }
+
+        fn bitsRemaining(self: *const Self) u8 {
+            if (self.byte == null) return 0;
+
+            return if (endian == .big)
+                self.bit_index + 1
+            else
+                8 - self.bit_index;
+        }
+    };
+}
+
+const testing = std.testing;
+
+test "bit stream" {
+    const bytes: [4]u8 = .{ 0b01001101, 0b11011000, 0b11110000, 0b10101010 };
+    var buf_stream = std.io.fixedBufferStream(&bytes);
+
+    var bit_stream_big = BitStream(@TypeOf(buf_stream).Reader, .big).init(buf_stream.reader());
+
+    try testing.expectEqual(0b010, try bit_stream_big.readBits(3));
+    try testing.expectEqual(0b01101, try bit_stream_big.readBits(5));
+    try testing.expectEqual(0b1, try bit_stream_big.readBits(1));
+    try testing.expectEqual(0b10, try bit_stream_big.readBits(2));
+    try testing.expectEqual(0b1, try bit_stream_big.readBits(1));
+    try testing.expectEqual(0b1000, try bit_stream_big.readBits(4));
+    try testing.expectEqual(0b11110000, try bit_stream_big.readBits(8));
+    try testing.expectEqual(0b10, try bit_stream_big.readBits(2));
+    try testing.expectEqual(0b10, try bit_stream_big.readBits(2));
+    try testing.expectEqual(0b10, try bit_stream_big.readBits(2));
+    try testing.expectEqual(0b10, try bit_stream_big.readBits(2));
+
+    buf_stream.reset();
+    var bit_stream_little = BitStream(@TypeOf(buf_stream).Reader, .little).init(buf_stream.reader());
+
+    try testing.expectEqual(0b101, try bit_stream_little.readBits(3));
+    try testing.expectEqual(0b10010, try bit_stream_little.readBits(5));
+    try testing.expectEqual(0b0, try bit_stream_little.readBits(1));
+    try testing.expectEqual(0b00, try bit_stream_little.readBits(2));
+    try testing.expectEqual(0b1, try bit_stream_little.readBits(1));
+    try testing.expectEqual(0b1011, try bit_stream_little.readBits(4));
+    try testing.expectEqual(0b00001111, try bit_stream_little.readBits(8));
+    try testing.expectEqual(0b01, try bit_stream_little.readBits(2));
+    try testing.expectEqual(0b01, try bit_stream_little.readBits(2));
+    try testing.expectEqual(0b01, try bit_stream_little.readBits(2));
+    try testing.expectEqual(0b01, try bit_stream_little.readBits(2));
+}
